13a81323137ba54d964d5edcb469f0c45d67e461
Glendon <glendon.pable@gmail.com>
2020-08-14 15:58:36 +0800

    MON-5_Added delete functionality

--- app/src/main/java/com/fmpdroid/moneybox/MainActivity.java
+++ app/src/main/java/com/fmpdroid/moneybox/MainActivity.java
@@ -1,37 +1,77 @@
 package com.fmpdroid.moneybox;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.fmpdroid.moneybox.adapter.ViewPagerAdapter;
+import com.fmpdroid.moneybox.dto.MoneyBoxDto;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int REQUEST_CODE = 1;
+    private Toolbar toolbar;
     private DrawerLayout drawer;
+    private ViewPagerAdapter adapter;
+    private ViewPager2 viewPager;
+    private FloatingActionButton fabAdd;
+    private FloatingActionButton fabDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_viewpager);
 
-        if (getIntent().getBooleanExtra(this.getResources().getString(R.string.key_isEmpty), true)) {
-            Intent myIntent = new Intent(this, EmptyActivity.class);
-            MainActivity.this.startActivity(myIntent);
-        } else {
-            goToDrawerScreen();
-        }
-
-    }
-
-    private void goToDrawerScreen() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setTitle(R.string.title_main_page);
+
+        viewPager = findViewById(R.id.viewPager);
+        fabAdd = findViewById(R.id.fabAdd);
+        fabDelete = findViewById(R.id.fabDelete);
+        adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                switch (state) {
+                    case ViewPager2.SCROLL_STATE_IDLE:
+                        fabAdd.show();
+                        fabDelete.show();
+                        break;
+                    case ViewPager2.SCROLL_STATE_DRAGGING:
+                    case ViewPager2.SCROLL_STATE_SETTLING:
+                        fabAdd.hide();
+                        fabDelete.hide();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void goToDrawerScreen() {
 
         drawer = findViewById(R.id.drawer_layout);
 
@@ -43,6 +83,41 @@ public class MainActivity extends AppCompatActivity {
                 new HomeFragment()).commit();
 
     }
+
+    public void addMoneyBox(View v) {
+        Intent myIntent = new Intent(this, CreateMoneyBox.class);
+        MainActivity.this.startActivityForResult(myIntent, REQUEST_CODE);
+    }
+
+    public void deleteMoneyBox(View v) {
+        if (adapter.getItemCount() > 0) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Confirm Delete");
+            alert.setMessage("Are you sure to delete this Money Box?");
+            alert.setPositiveButton("Yes", (dialogInterface, i) -> {
+                adapter.delete(viewPager.getCurrentItem());
+                dialogInterface.dismiss();
+                Toast.makeText(this, "Money Box successfully deleted", Toast.LENGTH_SHORT).show();
+            });
+            alert.setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss());
+            alert.show();
+        } else {
+            Toast.makeText(this, "Nothing to delete", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            MoneyBoxDto moneyBoxDto = data.getParcelableExtra(getResources().getString(R.string.key_moneybox));
+            adapter.add(moneyBoxDto);
+            viewPager.setCurrentItem(adapter.getItemCount());
+        }
+    }
+
+    /*
+    /*Will be used later
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -50,6 +125,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             finishAffinity();
         }
-    }
+    }*/
 
 }
--- app/src/main/java/com/fmpdroid/moneybox/dto/MoneyBoxDto.java
+++ app/src/main/java/com/fmpdroid/moneybox/dto/MoneyBoxDto.java
@@ -1,16 +1,57 @@
 package com.fmpdroid.moneybox.dto;
 
-import lombok.Getter;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-public @NoArgsConstructor class MoneyBoxDto {
+@NoArgsConstructor @Data
+public class MoneyBoxDto implements Parcelable {
 
-    private @Getter @Setter String Title;
-    private @Getter @Setter String Description;
-    private @Getter @Setter float TargetAmount;
-    private @Getter @Setter String TargetDate;
-    private @Getter @Setter boolean AllowReminder;
-    private @Getter @Setter String DateCreated;
+    private String name;
+    private String description;
+    private float remainingAmount;
+    private float targetAmount;
+    private String dateCreated;
+    private String targetDate;
+    private boolean allowReminder;
 
+    protected MoneyBoxDto(Parcel in) {
+        name = in.readString();
+        description = in.readString();
+        remainingAmount = in.readFloat();
+        targetAmount = in.readFloat();
+        dateCreated = in.readString();
+        targetDate = in.readString();
+        allowReminder = in.readByte() != 0;
+    }
+
+    public static final Creator<MoneyBoxDto> CREATOR = new Creator<MoneyBoxDto>() {
+        @Override
+        public MoneyBoxDto createFromParcel(Parcel in) {
+            return new MoneyBoxDto(in);
+        }
+
+        @Override
+        public MoneyBoxDto[] newArray(int size) {
+            return new MoneyBoxDto[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(description);
+        parcel.writeFloat(remainingAmount);
+        parcel.writeFloat(targetAmount);
+        parcel.writeString(dateCreated);
+        parcel.writeString(targetDate);
+        parcel.writeByte((byte) (allowReminder ? 1 : 0));
+    }
 }
