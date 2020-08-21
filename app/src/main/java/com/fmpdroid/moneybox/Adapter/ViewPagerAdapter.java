71924823729301b8dc7b8b720eb933a759848267
Glendon Pable <glendon.pable@gmail.com>
2020-08-22 00:23:40 +0800

    Mon 4 swipe functionality for multiple money boxes (#4)
    * MON-1 Initial Commit
    
    * MON-1_Changes from pull request
    
    * MON-2_Graphical Display after new money box is created
    
    * MON-2_Create Graphical Display changes from pull request
    
    * MON-3_Created screen to cater multiple graphical display
    
    * MON-3_gitignore fix
    
    * MON-3_Fixed corrupted files
    
    * MON-3_Removed .idea files
    
    * MON-3_Changes from pull request
    
    * MON-3_Changes from pull request v2
    
    * MON-4_Added Swipeable Functionality
    
    * MON-4_Removed unnecessary files, imports and refactored codes

--- app/src/main/java/com/fmpdroid/moneybox/Adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/fmpdroid/moneybox/Adapter/ViewPagerAdapter.java
@@ -29,8 +29,10 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
     @Override
     public Fragment createFragment(int position) {
         TextView textView = fa.findViewById(R.id.tv_empty);
-        int visibilityValue = fragments.size() == 0 ? View.VISIBLE : View.GONE;
-        textView.setVisibility(visibilityValue);
+        if (fragments.size() == 0)
+            textView.setVisibility(View.VISIBLE);
+        else
+            textView.setVisibility(View.GONE);
         return fragments.get(position);
     }
 
@@ -41,6 +43,7 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
 
     @Override
     public long getItemId(int position) {
+
         return super.getItemId(position);
     }
 
---
+++ app/src/main/java/com/fmpdroid/moneybox/ScreenSlidePageFragment.java
@@ -0,0 +1,51 @@
+package com.fmpdroid.moneybox;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.fmpdroid.moneybox.dto.MoneyBoxDto;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TextView tvMoneyBoxName = view.findViewById(R.id.tv_moneyBoxName);
+        TextView tvDescription = view.findViewById(R.id.tv_description);
+        TextView tvRemainingAmount = view.findViewById(R.id.tv_remainingAmount);
+        TextView tvTargetAmount = view.findViewById(R.id.tv_targetAmount);
+        TextView tvDateCreated = view.findViewById(R.id.tv_dateCreated);
+        TextView tvTargetDate = view.findViewById(R.id.tv_targetDate);
+
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            MoneyBoxDto moneybox = bundle.getParcelable(getResources().getString(R.string.key_moneybox));
+            tvMoneyBoxName.setText(moneybox.getName());
+            tvDescription.setText(moneybox.getDescription());
+            tvRemainingAmount.setText(String.valueOf(moneybox.getRemainingAmount()));
+            tvTargetAmount.setText(String.valueOf(moneybox.getTargetAmount()));
+            tvDateCreated.setText(moneybox.getDateCreated());
+            tvTargetDate.setText(moneybox.getTargetDate());
+        }
+
+        return view;
+    }
+
+    public static ScreenSlidePageFragment newInstance(MoneyBoxDto moneyBox, Context context) {
+        ScreenSlidePageFragment fragment = new ScreenSlidePageFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(context.getResources().getString(R.string.key_moneybox), moneyBox);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+}
